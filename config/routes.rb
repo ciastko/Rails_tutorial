dafc238a7bac89bbb853525a5617b58faec76a6f
Ciastko <ciastko@localhost>
2011-01-07 08:54:11 +0100

    Session management (Sign in/out)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 First::Application.routes.draw do
-  match '/signup',  :to => 'users#new'
 
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
@@ -8,8 +10,9 @@ First::Application.routes.draw do
   root :to => 'pages#home'
 
   resources :microposts
-
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -104,18 +104,12 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
   end
-
-  describe "GET '/'" do
-    it "should be successful" do
-      get '/'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get '/'
-      response.should have_selector("title", :content => "Home")
-    end
 
 end
